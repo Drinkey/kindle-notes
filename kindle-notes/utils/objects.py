62d7ab83da9d0a0c650a817dd88e579834af0364
Junkai Zhang @MacBookPro <junkai.zhang@watchguard.com>
2019-02-24 21:45:15 +0800

    feat(markdown): support markdown convert
    close #2
    close #3

--- kindle-notes/utils/objects.py
+++ kindle-notes/utils/objects.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta
+from enum import Enum
 
 class KindleNotes(metaclass=ABCMeta):
     def __init__(self):
@@ -18,3 +19,26 @@ class NoteElement(object):
     def __repr__(self):
         return "<section: ('{}'), heading: ('{}'), text: ('{}')>".format(
             self.section, self.heading, self.text)
+
+class MardownFormatter(Enum):
+    TITLE = "# {}\n\n"
+    AUTHOR = "> {}\n\n"
+    SECTION = "## {}\n\n"
+    S_HEADING = "### {}\n\n"
+    S_TEXT = "* {}\n"
+
+class KindleNotesMarkdown(object):
+    @staticmethod
+    def convert(notes: KindleNotes):
+        buffer = ''
+        buffer += MardownFormatter.TITLE.value.format(notes.title)
+        buffer += MardownFormatter.AUTHOR.value.format(notes.author)
+        for note in notes.notes:
+            buffer += MardownFormatter.SECTION.value.format(note.section) \
+                if note.section else ''
+            buffer += MardownFormatter.S_HEADING.value.format(note.heading) \
+                if note.heading else ''
+            buffer += MardownFormatter.S_TEXT.value.format(note.text) \
+                if note.text else ''
+            buffer += '\n'
+        return buffer
